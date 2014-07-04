b5c77b23a807f5cbbd842de8549365014fbb79a7
mario <mario@ubuntu>
2014-07-04 17:10:18 +0200

    part of the form with some errors(save to change branch and test some things)

---
+++ ballots/src/main/java/com/pfc/ballots/data/BallotKind.java
@@ -0,0 +1,6 @@
+package com.pfc.ballots.data;
+
+public enum BallotKind {
+
+	NORMAL,DOCENTE
+}
--- ballots/src/main/java/com/pfc/ballots/pages/ballot/CreateBallot.java
+++ ballots/src/main/java/com/pfc/ballots/pages/ballot/CreateBallot.java
@@ -1,6 +1,8 @@
 package com.pfc.ballots.pages.ballot;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.apache.tapestry5.ComponentResources;
@@ -11,13 +13,16 @@ import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.beaneditor.Validate;
 import org.apache.tapestry5.corelib.components.Zone;
+import org.apache.tapestry5.internal.plastic.Cache;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.Response;
 import org.apache.tapestry5.services.SelectModelFactory;
 import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
 
 import com.pfc.ballots.dao.CensusDao;
 import com.pfc.ballots.dao.FactoryDao;
+import com.pfc.ballots.data.BallotKind;
 import com.pfc.ballots.data.DataSession;
 import com.pfc.ballots.data.Method;
 import com.pfc.ballots.encoder.CensusEncoder;
@@ -39,15 +44,18 @@ public class CreateBallot {
 	
 	@Inject
 	private Request request;
-	
-	@Property
-	@Persist
-	private boolean showPage;
+
+
+
 	@InjectComponent
 	private Zone mainZone;
 
 	static final private String[] NUMBERS2_7 = new String[] { "2", "3", "4","5","6","7" };
 	
+	private enum Actions{
+		S_MAYREL,C_MAYREL
+	}
+	private Actions action;
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	 ////////////////////////////////////////////////////////// DAO //////////////////////////////////////////////////////////////////////
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -64,17 +72,21 @@ public class CreateBallot {
 	public void setupRender()
 	{
 		componentResources.discardPersistentFieldChanges();
+		
 		censusDao=DB4O.getCensusDao(datasession.getDBName());
 		censuses=censusDao.getByOwnerId(datasession.getId());
-	
+		showLoopMayRel=false;
+		showCensus=false;
+		showError=false;
 		if(censuses.size()==0)
 		{
-			showPage=false;
+			existCensus=false;
 		}
 		else
 		{
-			showPage=true;
+			existCensus=true;
 		}
+		selectedKind=false;
 		censusModel=selectModelFactory.create(censuses, "censusName");
 	}
 	
@@ -90,6 +102,9 @@ public class CreateBallot {
 	@Property
 	@Persist
 	private Method method;
+	@Property
+	@Persist
+	private BallotKind ballotKind;
 	
 	@Persist
 	private List<Census> censuses;
@@ -106,23 +121,39 @@ public class CreateBallot {
     private String dateInFormatStr = "dd/MM/yyyy";
 	@Property
 	@Persist
+	@Validate("regexp=^([0-2]?[0-9]|3[0-1])/([0-1][0-2]|0[0-9]|[0-9])/2[0-9][1-9][4-9]$")
 	private Date startDate;
 	@Property
 	@Persist
-	@Validate("regexp=([0-9]|[0-1][0-9]|2[0-3])\\:[0-5][0-9]\\:[0-5][0-9]")
+	@Validate("regexp=^([0-1]?[0-9]|2[0-3])\\:[0-5][0-9]\\:[0-5][0-9]$")
 	private String startHour;
 	@Property
 	@Persist
-	
+	@Validate("regexp=^([0-2]?[0-9]|3[0-1])/([0-1][0-2]|0[0-9]|[0-9])/2[0-9][1-9][4-9]$")
 	private Date endDate;
 	@Property
 	@Persist
-	@Validate("regexp=([0-9]|[0-1][0-9]|2[0-3])\\:[0-5][0-9]\\:[0-5][0-9]")
+	@Validate("regexp=^([0-1]?[0-9]|2[0-3])\\:[0-5][0-9]\\:[0-5][0-9]$")
 	private String endHour;
 	public CensusEncoder getCensusEncoder()
 	{
 	  return new CensusEncoder(censuses);
 	}
+
+	@Property
+	@Persist
+	private boolean showCensus;
+	@Property
+	@Persist
+	private boolean existCensus;
+	@Property
+	@Persist
+	private boolean selectedKind;
+	@Property
+	@Persist
+	private boolean showError;
+	
+	
 	
 	//Restore the values
 	private void notShowZones()
@@ -134,7 +165,6 @@ public class CreateBallot {
 	public void onValueChangedFromMethod(Method temp)
 	{
 		System.out.println("CHANGED->"+temp);
-		
 		notShowZones();
 		if(temp==null)
 		{
@@ -157,8 +187,39 @@ public class CreateBallot {
 		
 	 public void onValueChangedFromCensusSelect(Census temp)
 	 {
-		 System.out.println("CHANGED->"+temp.getCensusName());
+		
 	 }
+	 
+	 public void onValueChangedFromKind(BallotKind kind)
+	 {
+		 System.out.println("CHANGED->"+kind);
+		 
+		 //Comprueba si hay una opcion elegida
+		 if(kind==null)
+		 {
+			 selectedKind=false;
+		 }
+		 else
+		 {
+			selectedKind=true;			
+	 	 }
+		 
+		 //Comprueba que tipo de opcion esta elegida y si hay censos
+		 if(kind==BallotKind.NORMAL)
+		 {
+			 if(existCensus==false)
+			 {
+				 selectedKind=false;
+			 }
+			 showCensus=true;
+		 }
+		 else 
+		 {
+			 showCensus=false;
+		 }
+			ajaxResponseRenderer.addRender("mainZone",mainZone);
+	 }
+	 
 	  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	 ////////////////////////////////////////////////////// MAYORIA ZONE /////////////////////////////////////////////////////////////////
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -168,12 +229,18 @@ public class CreateBallot {
 	private boolean showMayoriaRel;
 	 
 	
+	@Persist
+	@Property
+	private boolean showLoopMayRel;
+	@Persist
+	@Property
+	private boolean showBadDate;
 	@Property
 	@Persist
 	private  String [] mayRelOpModel;
 
 	
-	
+	@Persist
 	@Property
 	private String mayRelOpNum;
 	@Property
@@ -182,27 +249,172 @@ public class CreateBallot {
 	@Property
 	@Persist
 	private String [] mayRelOps;
-	@Property
+	
 	private String mayRelOp;
+	
+	public String getMayRelOp()
+	{
+		return this.mayRelOp;
+	}
+	public void setMayRelOp(String mayRelOp)
+	{
+		System.out.println("aqui->"+mayRelOp+"num->"+mayRelOps.length);
+		this.mayRelOp=mayRelOp;
+	}
 	
 	public void onValueChangedFromMayRelNumSelect(String temp)
 	{
-		mayRelNum=Integer.parseInt(temp);
-		mayRelOps=new String[mayRelNum];
-		for(int i=0;i<mayRelOps.length;i++)
+		if(temp==null)
 		{
-			mayRelOps[i]=String.valueOf(i);
+			showLoopMayRel=false;
 		}
+		else
+		{
+			showLoopMayRel=true;
+			mayRelNum=Integer.parseInt(temp);
 			
+			mayRelOps=new String[mayRelNum];
+			
+		}	
 		System.out.println("NUM->"+mayRelNum);
 		ajaxResponseRenderer.addRender("mainZone",mainZone);
 	}
-	
-	
+	public void onSelectedFromSaveMayRel()
+	{
+		action=Actions.S_MAYREL;
+	}
+	public void onSelectedFromCancelMayRel()
+	{
+		action=Actions.C_MAYREL;
+	}
+
   	  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	 ////////////////////////////////////////////////////// KEMENY ZONE //////////////////////////////////////////////////////////////////
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	@Persist
 	@Property
 	private boolean showKemeny;
+	
+	  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	 ////////////////////////////////////////////////// VALIDATE AND SUCCESS /////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public void onValidateFromTotalForm()
+	{
+		showError=false;
+		showBadDate=false;
+		if(ballotName==null)
+		{
+			showError=true;
+		}
+		if(ballotKind==BallotKind.NORMAL)
+		{
+			Calendar cal1=new GregorianCalendar();;
+			Calendar cal2=new GregorianCalendar();;
+			Calendar cal3=new GregorianCalendar();;
+			if(census==null)
+			{
+				showError=true;
+			}
+			if(startDate==null)
+			{
+				startDate=new Date();
+			}
+			if(startHour!=null)
+			{
+				String [] temp=startHour.split(":");
+				
+				
+				cal1.setTime(startDate);
+				int hora=cal1.get(Calendar.HOUR_OF_DAY);
+				int min=cal1.get(Calendar.MINUTE);
+				int sec=cal1.get(Calendar.SECOND);
+				
+				cal1.add(Calendar.HOUR, Integer.parseInt(temp[0])-hora);
+				cal1.add(Calendar.MINUTE,Integer.parseInt(temp[1])-min);
+				cal1.add(Calendar.SECOND, Integer.parseInt(temp[2])-sec);
+				
+				startDate=cal1.getTime();
+				
+			}
+			if(endDate==null)
+			{
+				showError=true;
+			}
+			else if(endHour!=null)
+			{
+				String [] temp=startHour.split(":");
+				cal1.setTime(startDate);
+				cal2.setTime(endDate);
+				cal2.add(Calendar.HOUR, Integer.parseInt(temp[0]));
+				cal2.add(Calendar.MINUTE,Integer.parseInt(temp[1]));
+				cal2.add(Calendar.SECOND, Integer.parseInt(temp[2]));
+				cal3.setTime(new Date());
+				
+				if(cal2.before(cal1))
+				{
+					showBadDate=true;
+					
+				}
+				else if(cal2.before(cal3))
+				{
+					showBadDate=true;
+				}
+				
+			}
+			else
+			{
+				cal1.setTime(startDate);
+				cal2.setTime(endDate);
+				cal3.setTime(new Date());
+				if(cal2.before(cal1))
+				{
+					showBadDate=true;
+				}
+				if(cal2.before(cal3))
+				{
+					showBadDate=true;
+				}
+			}
+		}
+		else//VOTACION DOCENTE
+		{
+			startDate=new Date();
+			endDate=new Date();
+		}
+		
+		//COMPROBACIONES MAYORIA RELATIVA
+		if(method==Method.MAYORIA_RELATIVA)
+		{
+			for(String temp:mayRelOps)
+			{
+				System.out.println("CHECk"+temp);
+				if(temp==null)
+				{
+					showError=true;
+				}
+			}
+		}
+		
+	}
+	
+	
+	public void onSuccess()
+	{
+		if(showError|| showBadDate)
+		{
+			ajaxResponseRenderer.addRender("mainZone",mainZone);
+		}
+	}
+
+	
+	
+	  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	 ////////////////////////////////////////////////////// ONACTIVATE ///////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+		
+	public void onActivate()
+	{
+		
+	}
 }
