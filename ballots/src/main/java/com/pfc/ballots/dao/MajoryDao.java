0e2088a0ccd270331953f07cadeedf32db71feb3
violetamacaya <violetamacaya@gmail.com>
2014-12-15 15:13:09 +0100

    15 dic

---
+++ ballots/src/main/java/com/pfc/ballots/dao/MajoryDao.java
@@ -0,0 +1,17 @@
+package com.pfc.ballots.dao;
+
+import com.pfc.ballots.entities.MajoryText;
+
+/**
+ * Dao Interface to retrieves Majory entity
+ * @author Violeta Macaya Sánchez
+ * @version 2.0 DIC-2014
+ *
+ */
+public interface MajoryDao {
+	
+	public MajoryText getMajoryText();
+	public void deleteMajoryText();
+	public void updateMajoryText(MajoryText text);
+	
+}
---
+++ ballots/src/main/java/com/pfc/ballots/entities/KemenyText.java
@@ -0,0 +1,14 @@
+package com.pfc.ballots.entities;
+
+public class KemenyText {
+	
+	private String text;
+	
+	
+	public String getKemenyText() {
+		return text;
+	}
+	public void setKemenyText(String text) {
+		this.text = text;
+	}
+}
