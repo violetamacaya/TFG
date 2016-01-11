fefc42ae247192987f98bf061f7b401c93d01d0a
violetamacaya <violetamacaya@gmail.com>
2016-01-11 15:31:57 +0100

    Final

--- ballots/src/main/java/com/pfc/ballots/pages/Contact.java
+++ ballots/src/main/java/com/pfc/ballots/pages/Contact.java
@@ -1,94 +1,97 @@
-package com.pfc.ballots.pages;
+	package com.pfc.ballots.pages;
 
 
-import org.apache.tapestry5.ComponentResources;
-import org.apache.tapestry5.PersistenceConstants;
-import org.apache.tapestry5.annotations.Persist;
-import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.beaneditor.Validate;
-import org.apache.tapestry5.beaneditor.Width;
-import org.apache.tapestry5.ioc.annotations.Inject;
+	import org.apache.tapestry5.ComponentResources;
+	import org.apache.tapestry5.PersistenceConstants;
+	import org.apache.tapestry5.annotations.Persist;
+	import org.apache.tapestry5.annotations.Property;
+	import org.apache.tapestry5.beaneditor.Validate;
+	import org.apache.tapestry5.beaneditor.Width;
+	import org.apache.tapestry5.ioc.annotations.Inject;
 
-import com.pfc.ballots.dao.EmailAccountDao;
-import com.pfc.ballots.dao.FactoryDao;
-import com.pfc.ballots.entities.EmailAccount;
-import com.pfc.ballots.util.Mail;
+	import se.unbound.tapestry.breadcrumbs.BreadCrumb;
+	import se.unbound.tapestry.breadcrumbs.BreadCrumbReset;
 
-
-/**
- * Contact page of application ballots.
- */
-public class Contact
-{
-	
-	//////////////////////////////////////////////////////////// DAO ///////////////////////////////////////////////////////////
-	
-	FactoryDao DB4O =FactoryDao.getFactory(FactoryDao.DB4O_FACTORY);
-	EmailAccountDao emailAccountDao=null;
-	@Inject
-    private ComponentResources componentResources;
+	import com.pfc.ballots.dao.EmailAccountDao;
+	import com.pfc.ballots.dao.FactoryDao;
+	import com.pfc.ballots.entities.EmailAccount;
+	import com.pfc.ballots.util.Mail;
 
 
-	
-	
-	////////////////////////////////////////////////////////// INTIALIZE //////////////////////////////////////////////////////////////
-	
-	public void setupRender()
+	/**
+	 * Contact page of application ballots.
+	 */
+	public class Contact
 	{
-		showEmailProblem=false;
-		emailAccountDao=DB4O.getEmailAccountDao();
-		emailAccount=emailAccountDao.getAccount();
-		String mail = "inpo@usal.es";
-		emailAccount.setEmail(mail);
-		if(!Mail.checkAccount(emailAccount))
-		{
-			showEmailProblem=true;
-		}
+		
+		//////////////////////////////////////////////////////////// DAO ///////////////////////////////////////////////////////////
+		
+		FactoryDao DB4O =FactoryDao.getFactory(FactoryDao.DB4O_FACTORY);
+		EmailAccountDao emailAccountDao=null;
+		@Inject
+	    private ComponentResources componentResources;
+
 
 		
-	}
-	
-	@Property
-	@Persist(PersistenceConstants.FLASH)
-	EmailAccount emailAccount;
-	
-	@Property
-	@Persist(PersistenceConstants.FLASH)
-	private boolean showEmailProblem;
+		
+		////////////////////////////////////////////////////////// INTIALIZE //////////////////////////////////////////////////////////////
+		
+		public void setupRender()
+		{
+			showEmailProblem=false;
+			emailAccountDao=DB4O.getEmailAccountDao();
+			emailAccount=emailAccountDao.getAccount();
+			emailAccount.setEmail("inpo@usal.es");
+			if(!Mail.checkAccount(emailAccount))
+			{
+				showEmailProblem=true;
+			}
 
-	@Property 
+			
+		}
+		
+		@Property
+		@Persist
+		EmailAccount emailAccount;
+		
+		@Property
+		private boolean showEmailProblem;
+
+		@Property
+		@Validate("required,regexp=^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
+		@Persist(PersistenceConstants.FLASH)
+		private String remitente;
+		
+		@Width(value = 75)
+		@Property
+		@Validate("minLength=20")
+		@Persist(PersistenceConstants.FLASH)
+		private String text;
+		
+
+@Property 
 	@Persist(PersistenceConstants.FLASH)
 	private boolean sent;
 	@Property 
 	@Persist(PersistenceConstants.FLASH)
 	private boolean sendingError;
 	
-	@Property
-	@Validate("required,regexp=^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
-	@Persist(PersistenceConstants.FLASH)
-	private String remitente;
-	
-	@Width(value = 75)
-	@Property
-	@Validate("minLength=20")
-	@Persist(PersistenceConstants.FLASH)
-	private String text;
-	
-	
-	public void onSuccessFromContact()
-	{
+		public void onSuccessFromContact()
+		{
 
-		String asunto="Contact: "+remitente;
-		String asuntoRemit="NoReply-copia: "+emailAccount.getEmail();
-		if(Mail.sendMail(emailAccount.getEmail(), emailAccount.getPassword(), emailAccount.getEmail(), asunto, text))
-		{
-			Mail.sendMail(emailAccount.getEmail(), emailAccount.getPassword(), remitente, asuntoRemit, text);
-			sent=true;
+			String asunto="Contact: "+remitente;
+			String asuntoRemit="NoReply-copia: "+emailAccount.getEmail();
+			if(Mail.sendMail(emailAccount.getEmail(), emailAccount.getPassword(), emailAccount.getEmail(), asunto, text))
+			{
+				Mail.sendMail(emailAccount.getEmail(), emailAccount.getPassword(), remitente, asuntoRemit, text);
+				sent=true;
+			}
+			else
+			{
+				System.out.println("ERROR AL ENVIAR");
+				sendingError=true;
+			}
+
+			
 		}
-		else
-		{
-			sendingError=false;
-		}		
-		
 	}
-}
